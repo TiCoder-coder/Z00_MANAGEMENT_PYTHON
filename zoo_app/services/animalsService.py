f0bd6a52d2f251e5d1c1f4066036862911c77728
khoi <khoinguyen.23062003@gmail.com>
2025-10-11 13:38:31 +0700

    Update animalsService.py

--- zoo_app/services/animalsService.py
+++ zoo_app/services/animalsService.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from django.utils import timezone
 from zoo_app.models import Animals, Enclosures
 from zoo_app.serializers.createAnimalDto import CreateAnimalDto
 from zoo_app.serializers.updateAnimalDto import UpdateAnimalDto
@@ -9,106 +10,168 @@ class animalService:
     # Tạo động vật mới.
     def createAnimals(self, dto: CreateAnimalDto):
         # Input: dto (CreateAnimalDto)
-        # Kiểm tra enclusure có tồn tại không.
-        try:
-            enclosure = Enclosures.object.get(idEnclosure=dto.enclusoreId)
-        except ObjectDoesNotExist:
-            raise Exception(f"Enclosure with id {dto.enclosureId} not found")
-        # Kiểm tra id của animal có trùng khi thêm vào animal mới không.
-        if Animals.object.filter(id=dto.id).exists():
-            raise Exception(f"Animal wit id {dto.id} already exists")
-        # Tạo đối tượng animal mới.
-        animal = Animals(
-            id=dto.id,
-            name=dto.name,
-            age=dto.age,
-            species=dto.species,
-            gender=dto.gender,
-            weight=dto.weight,
-            healthStatus=dto.healthStatus,
-            enclosureId=enclosure.idEnclosure,
-            createAt=datetime.now(),
-            updateAt=datetime.now()
-        )
-        animal.save()  # Lưu vào mongodb
-        # Output ra json
-        return {
-            "message": "Animal created successfully",
-            "data": {
-                "id": animal.id,
-                "name": animal.name,
-                "species": animal.species,
-                "enclosureId": animal.enclosureId
-            }
-        }
 
-    # Xuất toàn bộ danh sách animal
+        # Thêm try/except tránh vỡ API.
+        try:
+            enclosure = Enclosures.objects.get(idEnclosure=dto.enclosureId)
+            # Kiểm tra trùng id animal
+            if Animals.objects.filter(id=dto.id).exists():
+                return {
+                    "status": "error",
+                    "message": f"Animal with id {dto.id} already exists"
+                }
+            animal = Animals(
+                id=dto.id,
+                name=dto.name,
+                age=dto.age,
+                species=dto.species,
+                gender=dto.gender,
+                weight=dto.weight,
+                healthStatus=dto.healthStatus,
+                enclosureId=dto.enclosureId,
+                createAt=timezone.now(),
+                updateAt=timezone.now()
+            )
+            # Lưu vào database.
+            animal.save()
+
+            # Sửa lại đoạn này return toàn bộ thông tin.
+            return {
+                "status": "success",
+                "data": {
+                    "id": animal.id,
+                    "name": animal.name,
+                    "age": animal.age,
+                    "species": animal.species,
+                    "gender": animal.gender,
+                    "weight": animal.weight,
+                    "healthStatus": animal.healthStatus,
+                    "enclosureId": animal.enclosureId,
+                    # Chuyển dữ liệu sang str để phù hợp trong json.
+                    "createAt": str(animal.createAt),
+                    "updateAt": str(animal.updateAt)
+                }
+            }
+        except ObjectDoesNotExist:
+            return {
+                "status": "error",
+                "message": f"Enclosure with id {dto.enclosureId} not found"
+            }
+        except Exception as e:
+            # Luôn trẻ về json khi có lỗi.
+            return {
+                "status": "error",
+                "message": str(e)
+            }
+
+    # Lấy danh sách animal
     def reviewAnimals(self):
-        """Lấy toàn bộ danh sách animal trong database
-           Nếu không có animal nào thì trả về rỗng."""
-        animasl = Animals.object.all()
-        result = []
-        # Dùng vòng lặp để duyệt qua từng animal và đưa vào list
-        for ani in animasl:
-            result.append({
-                "id": ani.id,
-                "name": ani.name,
-                "species": ani.species,
-                "gender": ani.gender,
-                "healthStatus": ani.healthStatus
-            })
-        return result
-
-    # Cập nhật thông tin cho animal
-    def updateAnimals(self, id: str, dto: UpdateAnimalDto):
-        # Input: id, dto.
-        # Kiểm tra animal có tồn tại không.
+        # Sửa đoạn này thêm try/except
         try:
-            animal = Animals.object.get(id=id)
-        except ObjectDoesNotExist:
-            raise Exception(f"Animal with id {id} not found")
+            animals = Animals.objects.all()
+            data = []
+            for ani in animals:
+                data.append({
+                    "id": ani.id,
+                    "name": ani.name,
+                    "age": ani.age,
+                    "species": ani.species,
+                    "gender": ani.gender,
+                    "weight": ani.weight,
+                    "healthStatus": ani.healthStatus,
+                    "enclosureId": ani.enclosureId,
+                    "createAt": str(ani.createAt),
+                    "updateAt": str(ani.updateAt)
+                })
 
-        # Nếu enclosure có trong dot, kiểm tra sự tồn tại.
-        if dto.enclosureId:
-            if not Enclosures.object.filter(idEnclosure=dto.enclosureId).exists():
-                raise Exception(
-                    f"Enclosure with id {dto.enclosureId} not found")
-        # Cập nhật từng field nếu dto có giá trị. Kiểm tra để tánh None
-        if dto.name:
-            animal.name = dto.name
-        if dto.age:
-            animal.age = dto.age,
-        if dto.species:
-            animal.species = dto.species,
-        if dto.gender:
-            animal.gender = dto.gender,
-        if dto.weight:
-            animal.weight = dto.weight,
-        if dto.healthStatus:
-            animal.healthStatus = dto.healthStatus
-        if dto.enclosureId:
-            animal.enclosureId = dto.enclosureId
-
-        # Cập nhật thời gian thay đổi
-        animal.updatAt = datetime.now()
-        animal.save()
-
-        # id: mã con vật vừa mới update
-        # updateAt: thời gian update. Ép thành chuối string để đưa vào JSON
-        return {
-            "message": "Animal updated successfully",
-            "data": {
-                "id": id,
-                "updateAt": str(animal.updateAt)
+            return {
+                "status": "success",
+                "data": data
             }
-        }
+        except Exception as e:
+            return {
+                "status": "error",
+                "message": str(e)
+            }
 
-    def deleteAnimal(self, id: str):
-        # Input: id
-        # Kiểm tra animal có tồn tại không
+    def updateAnimals(self, id: str, dto: UpdateAnimalDto):
         try:
-            animal = Animals.object.get(id=id)
-            animal.delete()
-            return {"message": f"Animal with id {id} deleted successfully"}
+            animal = Animals.objects.get(id=id)
+            # Kiểm tra enclosure có tồn tại không.
+            if dto.enclosureId and not Enclosures.objects.filter(idEnclosure=dto.enclosureId).exists():
+                return {
+                    "status": "error",
+                    "message": f"Enclosure with id {dto.enclosureId} not found"
+                }
+
+            if dto.name:
+                animal.name = dto.name
+            if dto.age:
+                animal.age = dto.age
+            if dto.species:
+                animal.species = dto.species
+            if dto.gender:
+                animal.gender = dto.gender
+            if dto.weight:
+                animal.weight = dto.weight
+            if dto.healthStatus:
+                animal.healthStatus = dto.healthStatus
+            if dto.enclosureId:
+                animal.enclosureId = dto.enclosureId
+
+            animal.updateAt = timezone.now()
+            animal.save()
+
+            # return toàn bộ thông tin sau khi update
+            return {
+                "status": "success",
+                "data": {
+                    "id": animal.id,
+                    "name": animal.name,
+                    "age": animal.age,
+                    "species": animal.species,
+                    "gender": animal.gender,
+                    "weight": animal.weight,
+                    "healthStatus": animal.healthStatus,
+                    "enclosureId": animal.enclosureId,
+                    "createAt": str(animal.createAt),
+                    "updateAt": str(animal.updateAt)
+                }
+            }
+
         except ObjectDoesNotExist:
-            raise Exception(f"Animal with id {id} not found")
+            return {
+                "status": "error",
+                "message": f"Animal with id {id} not found"
+            }
+        except Exception as e:
+            return {
+                "status": "error",
+                "message": str(e)  # Thông báo lỗi trong json.
+            }
+
+    def deleteAnimals(self, id: str):
+        try:
+            animal = Animals.objects.get(id=id)
+            animal.delete()
+
+            # Thêm return trả về thông tin khi xóa thành công.
+            return {
+                "status": "success",
+                "message": f"Animal with id {id} delete successfully",
+                "data": {
+                    "deleteId": id,
+                    "deleteName": animal.name,
+                    "species": animal.species
+                }
+            }
+        except ObjectDoesNotExist:
+            return {
+                "status": "error",
+                "message": f"Animal with id {id} not found"
+            }
+        except Exception as e:
+            return {
+                "status": "error",
+                "message": str(e)
+            }
