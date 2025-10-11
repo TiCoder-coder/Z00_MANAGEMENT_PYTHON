3089fbc856f651a234cb155c4eb92c0cc05c0db4
khoi <khoinguyen.23062003@gmail.com>
2025-10-11 13:40:35 +0700

    Update managersService.py

--- zoo_app/services/managersService.py
+++ zoo_app/services/managersService.py
@@ -8,99 +8,186 @@ from zoo_app.serializers.updateManagerDto import UpdateManagerDto
 class managersService:
     # Tạo manager mới (chỉ manager được thực hiện)
     def createManager(self, dto: CreateManagerDto) -> Dict[str, Any]:
-        # Kiểm tra userName đã tồn tại trong database chưa.
-        existing_user = Managers.object.filter(userName=dto.userName).first()
-        if existing_user is not None:
-            # Tìm thấy user có userName trùng lặp thì báo lỗ và dừng lại.
-            raise ValueError(f"The login name {dto.userName} already exists")
-        # Kiểm tra id có bị trùng không
-        existing_id = Managers.object.filter(id=dto.id).first()
-        if existing_id is not None:
-            raise ValueError(f"Manager has Id {dto.id} already exists")
-        # Mã hóa mật khẩu trước khi lưu
-        hashed_password = make_password(dto.password)
-        # Tạo object Maneger và gán giá trị
-        manager = Managers(
-            id=dto.id,
-            name=dto.name,
-            userName=dto.userName,
-            password=hashed_password,  # Lưu hash
-            role=dto.role
-        )
+        # Thêm try/except
+        try:
+            # Kiểm tra userName đã tồn tại trong database chưa.
+            existing_user = Managers.objects.filter(
+                userName=dto.userName).first()
+            if existing_user:
+                return {
+                    "status": "error",
+                    "message": f"The login name {dto.userName} already exists",
+                    "data": None
+                }
+            # Kiểm tra id có trùng không.
+            existing_id = Managers.objects.filter(id=dto.id).first()
+            if existing_id:
+                return {
+                    "status": "error",
+                    "message": f"Manager with id {dto.id} already exists",
+                    "data": None
+                }
 
-        # Lưu vào database
-        manager.save()
+            # Mã hóa mật khẩu.
+            hashes_password = make_password(dto.password)
+            # Tạo object mới cho class Manager.
+            manager = Managers(
+                id=dto.id,
+                name=dto.name,
+                userName=dto.userName,
+                password=hashes_password,
+                role=dto.role
+            )
 
-        # Trả về thông tin đã tạo (không để password để bảo vệ)
-        return {
-            "message": "Created manager successful",
-            "data": manager.to_dict()
-        }
+            manager.save()  # Lưu vào database.
 
-    def reviewManager(self) -> List[Dict[str, Any]]:
-        """Lấy toàn bộ danh sách manager trong database
-           Trả về dạng dữ liệu List of Dict
-           Không trả về password."""
-        managers = Managers.object.all()
-        if not managers:
-            return []
-        # Duyệt từng Managers, chuyển thành dict và thêm list vào result.
-        result_list = []
-        for manager in managers:
-            # Mỗi lần duyệt thêm vào danh sách.
-            manager_inf = manager.to_dict()
-            result_list.append(manager_inf)
+            # Lấy data và lưu data dạng dict.
+            data = manager.to_dict()
 
-        # Trả về danh sách đầy đủ sau khi duyệt xong.
-        return result_list
+            # Ẩn password và userName.
+            if "password" in data:
+                data.pop("password")
+            if "userName" in data:
+                data.pop("userName")
 
-    def updateManager(self, id: str, dto: UpdateManagerDto) -> Optional[Dict[str, Any]]:
-        # Cập nhất thông tin manager có id tương ứng
-        # Tìm Manager có id tương ứng.
-        manager = Managers.object.fliter(id=id).first()
-        # Không tìm thấy trả về None
-        if manager is None:
-            return None
+            # Trả về return.
+            return {
+                "status": "success",
+                "message": "Manager created successfully",
+                "data": data
+            }
+        except Exception as e:
+            # Trả về thông báo khi có lỗi.
+            return {
+                "status": "error",
+                "message": f" Error creating manager: {str(e)}",
+                "data": None
+            }
 
-        # Có userName mới kiểm tra đã có ai sử dụng chưa.
-        if dto.userName is not None:
-            if dto.userName != manager.userName:
-                # Kiểm tra có manager đã sử dụng userName này chưa.
-                conflict_user = Managers.object.filter(
-                    userName=dto.userName).exclude(id=id).first()
-                if conflict_user is not None:
-                    # Nếu có báo lỗi.
-                    raise ValueError(
-                        f"The login {dto.userName} already exists")
-                # Nếu không trùng thì cập nhật userName mới
-                manager.userName = dto.userName
-        # Nếu có password mới thì hash lại
-        if dto.password is not None:
-            hashed_pw = make_password(dto.password)
-            manager.password = hashed_pw
-        # Nếu có name mới thì cập nhật
-        if dto.name is not None:
-            manager.name = dto.name
-        # Nếu có role mới thì cập nhật
-        if dto.role is not None:
-            manager.role = dto.role
+    def reviewManager(self) -> Dict[str, Any]:
+        # Thêm try/except
+        try:
+            # Lấy toàn bộ danh sách manager.
+            managers = Managers.objects.all()
+            # Không có manager nào thì trả về danh sách rỗng.
+            if not managers.exists():
+                return {
+                    "status": "error",
+                    "message": "There is no managers in the system",
+                    "data": []
+                }
 
-        # Lưu các thay đổi vào database
-        manager.save()
+            result_list = []
+            # Duyệt qua từng managers
+            for m in managers:
+                # Chuyển thàng dict cho dễ đọc.
+                inf = m.to_dict()
 
-        # Trả về result không hiện password
-        return {
-            "message": "Manager updated successful",
-            "data": manager.to_dict()
-        }
+                # Ẩn userName và password.
 
-    def deleteManager(self, id: str) -> bool:
-        # Xóa manager theo id
-        # Tìm manager có id bạn muốn del
-        manager = Managers.object.filter(id=id).first()
-        # Không tìm thấy trả về False.
-        if manager is None:
-            return False
-        # Nếu tìm thấy thì xóa và trả về True
-        manager.delete()
-        return True
+                if "password" in inf:
+                    inf.pop("password")
+                if "userName" in inf:
+                    inf.pop("userName")
+
+                result_list.append(inf)  # Thêm kết quả vào result.
+
+            # Trả về all class Manager.
+            return {
+                "status": "success",
+                "message": "Get manager list successfully",
+                "data": result_list
+            }
+
+        except Exception as e:
+            # Nếu có lỗi trả về thông tin lỗi.
+            return {
+                "status": "error",
+                "message": f"Error when getting manager: {str(e)}",
+                "data": []
+            }
+
+    def updateManager(self, id: str, dto: UpdateManagerDto) -> Dict[str, Any]:
+        # Thêm try/except.
+        try:
+            # Tìm manager có id tương ứng.
+            manager = Managers.objects.filter(id=id).first()
+            # Không thấy trả về lỗi.
+            if manager is None:
+                return {
+                    "status": "error",
+                    "message": f"Manager with id {id} not found",
+                    "data": None
+                }
+            # Nếu có userName mới kiểm tra có trùng không.
+            if dto.userName is not None:
+                if dto.userName != manager.userName:
+                    # Kiểm trong database của manager có trùng userName muốn cập nhật không.
+                    conflict_user = Managers.objects.filter(
+                        userName=dto.userName).exclude(id=id).first()
+                    if conflict_user is not None:
+                        return {
+                            "status": "error",
+                            "message": f"The login name {dto.userName} is already exists",
+                            "data": None
+                        }
+                    # Cạp nhật userName mới.
+                    manager.userName = dto.userName
+            # Cập nhật password mới thì hash password
+            if dto.password is not None:
+                manager.password = make_password(dto.password)
+            # Cập nhật tên mới.
+            if dto.name is not None:
+                manager.name = dto.name
+            # Cập nhật vai trò mới.
+            if dto.role is not None:
+                manager.role = dto.role
+            manager.save()  # Lưu lại.
+            data = manager.to_dict()
+            # Ẩn userName và password.
+            if "password" in data:
+                data.pop("password")
+            if "userName" in data:
+                data.pop("userName")
+
+            # Trả về thông tin.
+            return {
+                "status": "success",
+                "message": "Manager information updated successfully",
+                "data": data
+            }
+        except Exception as e:
+            return {
+                "status": "error",
+                "message": f"Error updating manager: {str(e)}",
+                "data": None
+            }
+
+    def deleteManager(self, id: str) -> Dict[str, Any]:
+        # Thêm try/except
+        try:
+            # Tìm manager càn xóa.
+            manager = Managers.objects.filter(id=id).first()
+            # Không tìm thấy thì none
+            if manager is None:
+                return {
+                    "status": "error",
+                    "message": f"Manager with id {id} not found",
+                    "data": None
+                }
+            # Tìm thấy thì xóa.
+            manager.delete()
+
+            # Trả về xác nhận sau khi xóa.
+            return {
+                "status": "success",
+                "message": "Manager deleted successfully",
+                "data": None
+            }
+
+        except Exception as e:
+            return {
+                "status": "error",
+                "message": f"Error deleting manager: {str(e)}",
+                "data": None
+            }
