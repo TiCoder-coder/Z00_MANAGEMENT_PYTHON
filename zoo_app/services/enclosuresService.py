d049043cfd503e8ee11fa13deaa57293cce6d12c
khoi <khoinguyen.23062003@gmail.com>
2025-10-11 13:39:17 +0700

    Update enclosuresService.py

--- zoo_app/services/enclosuresService.py
+++ zoo_app/services/enclosuresService.py
@@ -1,4 +1,5 @@
 from zoo_app.models import Enclosures
+from django.core.exceptions import ObjectDoesNotExist
 from zoo_app.serializers.createEnclosureDto import CreateEnclosureDto
 from zoo_app.serializers.updateEnclosureDto import UpdateEnclosureDto
 
@@ -6,87 +7,135 @@ from zoo_app.serializers.updateEnclosureDto import UpdateEnclosureDto
 class enclosureService:
     # Hàm tạo enclosure(chuồng) mới.
     def createEnclosure(self, dto: CreateEnclosureDto):
-        # Kiểm tra enclosure đã tồn tại trong database chưa.
-        # Nếu enclosire tồn tại trả về giá trị đầu idEnclosure trong Enclosure.
-        existed = Enclosures.object.filter(idEnclosure=dto.idEnclosure).first()
-        # Không có gì thì trả về rỗng.
-        if existed:
-            raise ValueError(
-                f"Enclosure with id {dto.idEnclosure} already exists")
-        # Chưa tồn tại thì tạo object Enclosure
-        enclosure = Enclosures(
-            idEnclosure=dto.idEnclosure,
-            nameEnclosure=dto.nameEnclosure,
-            areaSize=dto.areaSize,
-            climate=dto.climate,
-            capacity=dto.capacity
-        )
-        # Lưu vào database
-        enclosure.save()
-        # Trả về dữ liệu về dạng dict
-        return {
-            "idEnclosure": enclosure.idEnclosure,
-            "nameEnclosure": enclosure.nameEnclosure,
-            "areaSize": enclosure.areaSize,
-            "climate": enclosure.climate,
-            "capacity": enclosure.capacity
-        }
+        # Thêm try/except
+        try:
+            existed = Enclosures.objects.filter(
+                idEnclosure=dto.idEnclosure).first()
+            if existed:
+                return {
+                    "status": "error",
+                    "message": f"Enclosure with id {dto.idEnclosure} already exists"
+                }
 
-    # Lấy toàn bộ danh sách enclosure trong database
+            enclosure = Enclosures(
+                idEnclosure=dto.idEnclosure,
+                nameEnclosure=dto.nameEnclosure,
+                areaSize=dto.areaSize,
+                climate=dto.climate,
+                capacity=dto.capacity
+            )
+            enclosure.save()  # Lưu vào database
+
+            # Thêm return toàn bộ thông tin.
+            return {
+                "status": "success",
+                "data": {
+                    "idEnclosure": enclosure.idEnclosure,
+                    "nameEnclosure": enclosure.nameEnclosure,
+                    "areaSize": enclosure.areaSize,
+                    "climate": enclosure.climate,
+                    "capacity": enclosure.capacity
+                }
+            }
+
+        except Exception as e:
+            return {
+                "status": "error",
+                "message": str(e)
+            }
+
     def reviewEnclosure(self):
-        # Lấy toàn bộ dữ liệu trong collection Enclosure
-        enclosures = Enclosures.object.all()
-        # Nếu không có enclosures thì trả về mảng rỗng.
-        if not enclosures:
-            return []
-        # Duyệt qua từng enclosures và chuyển thành dạng dữ liệu dict
-        result = []
-        for e in enclosures:
-            result.append({
-                "idEnclosure": e.idEclosure,
-                "nameEnclosure": e.nameEnclosure,
-                "areaSize": e.areaSize,
-                "climate": e.climate,
-                "capacity": e.capacity
-            })
-        return result
+        # Thêm try/except
+        try:
+            enclosures = Enclosures.objects.all()
+            data = []
+
+            for e in enclosures:
+                data.append({
+                    "idEnclosure": e.idEnclosure,
+                    "nameEnclosure": e.nameEnclosure,
+                    "areaSize": e.areaSize,
+                    "climate": e.climate,
+                    "capacity": e.capacity
+                })
+            return {
+                "status": "success",
+                "data": data
+            }
+        except Exception as e:
+            return {
+                "status": "error",
+                "message": str(e)
+            }
 
     def updateEnclosure(self, idEnclosure: str, dto: UpdateEnclosureDto):
-        # Cập nhật thông tin cho 1 enclosure bằng idEnclosure
-        # chỉ cập nhật những trường nào có trong dto
+        # Thêm try/except
+        try:
+            enclosure = Enclosures.objects.filter(
+                idEnclosure=idEnclosure).first()
+            if not enclosure:
+                return {
+                    "status": "error",
+                    "message": f"Enclosure with id {idEnclosure} not found"
+                }
+            if dto.nameEnclosure:
+                enclosure.nameEnclosure = dto.nameEnclosure
+            if dto.areaSize:
+                enclosure.areaSize = dto.areaSize
+            if dto.climate:
+                enclosure.climate = dto.climate
+            if dto.capacity:
+                enclosure.capacity = dto.capacity
 
-        # Tìm enclosure cần update
-        enclosure = Enclosures.object.filter(idEnclosure=idEnclosure).first()
-        if not enclosure:
-            return None  # Không tìm thấy
-        """Cập nhật từng filed nếu dto có giá trị
-           Nếu dto không có giá trị thì bỏ qua nó."""
-        if dto.nameEnclosure:
-            enclosure.nameEnclosure = dto.nameEnclosure
-        if dto.areaSize:
-            enclosure.areaSize = dto.areaSize
-        if dto.climate:
-            enclosure.climate = dto.climate
-        if dto.capacity:
-            enclosure.capacity = dto.capacity
+            enclosure.save()
 
-        enclosure.save()  # Lưu thay đổi.
+            # return toàn bộ thông tin.
+            return {
+                "status": "success",
+                "data": {
+                    "idEnclosure": enclosure.idEnclosure,
+                    "nameEnclosure": enclosure.nameEnclosure,
+                    "areaSize": enclosure.areaSize,
+                    "climate": enclosure.climate,
+                    "capacity": enclosure.capacity
+                }
+            }
+        except Exception as e:
+            return {
+                "status": "error",
+                "message": str(e)
+            }
 
-        # Trả về dữ liệu sau khi update
-        return {
-            "idEnclosure": enclosure.idEnclosure,
-            "nameEnclosure": enclosure.nameEnclosure,
-            "areaSize": enclosure.areaSize,
-            "climate": enclosure.climate,
-            "capacity": enclosure.capacity
-        }
+    def deleteEnclosure(self, idEnclosure: str):
+        try:
+            enclosure = Enclosures.objects.filter(
+                idEnclosure=idEnclosure).first()
+            if not enclosure:
+                return {
+                    "status": "error",
+                    "message": f"Enclosure with id {idEnclosure} not found"
+                }
 
-    def deleteEnclosure(sele, idEnclosure: str):
-        # Xóa 1 enclosure bằng idEnclosure
-        # Kiểm tran enclosure có tồn tại không.
-        enclosure = Enclosures.ogject.filter(idEnclosure=idEnclosure).first()
-        if not enclosure:
-            return False
-        # Xóa nếu enclosure tồn tại
-        enclosure.delete()
-        return True
+            # Lưu thông tin trả về sau khi del
+            delete_inf = {
+                "deleteId": enclosure.idEnclosure,
+                "deleteName": enclosure.nameEnclosure,
+                "areaSize": enclosure.areaSize,
+                "climate": enclosure.climate,
+                "capacity": enclosure.capacity
+            }
+
+            enclosure.delete()  # Lưu vào database
+
+            # Thêm return chi tiết sau khi xóa thành công.
+            return {
+                "status": "success",
+                "message": f"Enclosure with id {idEnclosure} delete successfully",
+                "data": delete_inf
+            }
+
+        except Exception as e:
+            return {
+                "status": "error",
+                "message": str(e)
+            }
